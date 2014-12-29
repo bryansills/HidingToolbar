3bdc47924f13cf477da65b58a8f490b98709bb7a
Bryan Sills <bsills@sapient.com>
2014-12-29 16:21:42 -0600

    added theme stuff and a working listview.

--- app/src/main/java/com/bryansills/hidingtoolbar/MainActivity.java
+++ app/src/main/java/com/bryansills/hidingtoolbar/MainActivity.java
@@ -1,17 +1,36 @@
 package com.bryansills.hidingtoolbar;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mListView = (ListView) findViewById(R.id.listview);
+
+        List<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
     }
 
 
